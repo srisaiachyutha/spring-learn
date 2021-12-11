af91a418d398add47987d6fa522f74ee1c3d7c81
achyutha <srisaiachyutha@gmail.com>
2021-12-11 10:43:28 +0530

    initializing the project

---
+++ src/main/java/io/achyutha/achyutha/AchyuthaApplication.java
@@ -0,0 +1,13 @@
+package io.achyutha.achyutha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchyuthaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchyuthaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/achyutha/achyutha/AchyuthaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.achyutha.achyutha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchyuthaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
